4d063e43fc3d1f661e761e85819394ed7af815ff
Elisa Jorge <ejorgon@gobiernodecanarias.org>
2026-07-09 17:50:02 +0000

    version 0.3.1

--- R/statistical-resources-cubes.R
+++ R/statistical-resources-cubes.R
@@ -69,25 +69,29 @@ get_statisticalresources_datasets_agency_resource <- function(agencyid, resource
 #' @title Get datasets (agencyID / resourceID / version)
 #' @description This function allows to obtain a certain version of a statistical cube with a certain
 #' identifier and that also maintains a certain organization.
-#' @param agencyid (string): Identifier of the maintainer organization of the resource. A possible value is ``ISTAC``.
-#' @param resourceid (string): Resource identifier. A possible value is ``C00010A_000002``.
-#' @param version (string): Resource version. A possible value is ``001.000``.
-#' @param dim (string): Allows filtering the data obtained in the response. A example is ``TIME_PERIOD:2009|2010``.
-#' @param fields (string): Allows you to customize the response by excluding fields from it. The possible values are ``-metadata`` and ``-data``.
+#' @param agencyid (string): Identifier of the maintainer organization of the resource. A possible value is `ISTAC`.
+#' @param resourceid (string): Resource identifier. A possible value is `C00010A_000002`.
+#' @param version (string): Resource version. A possible value is `001.000`.
+#' @param dim (string): Allows filtering the data obtained in the response. An example is `TIME_PERIOD:2009|2010`.
+#' @param representation (string): Specifies the representation format of the returned data. Leave empty to use the API default.
+#' @param granularity (string): Specifies the desired level of aggregation or granularity for the returned data. Leave empty to use the API default.
+#' @param fields (string): Allows you to customize the response by excluding fields from it. The possible values are `-metadata` and `-data`.
 #' @param lang (string): Language in which you want to get the answer.
-#' @param as_dataframe (bool): If True, this function returns a namedtuple with:
-#'   - dataframe: pandas dataframe built from API response.
+#' @param as_dataframe (bool): If TRUE, this function returns a list with:
+#'   - dataframe: data.frame built from the API response.
 #'   - codelists: mapping between codes and representations for each column.
+#'
 #' @examples
 #' get_statisticalresources_datasets_agency_resource_version(
-#' agencyid="ISTAC",
-#' resourceid="E30050A_000007",
-#' version="~latest",
-#' dim="TIME_PERIOD:2025-M01:MEDIDAS:IPI_BASE_2021:TERRITORIO:ES70")
+#'   agencyid = "ISTAC",
+#'   resourceid = "E68025A_000007",
+#'   version = "~latest",
+#'   representation = "TIME_PERIOD[~last=1]:MEDIDAS[CASO_ACTIVO]:TERRITORIO[ES70]")$dataframe
+#'
 #' @export
-get_statisticalresources_datasets_agency_resource_version <- function(agencyid, resourceid, version, dim='', fields='', lang='es', as_dataframe=T) {
+get_statisticalresources_datasets_agency_resource_version <- function(agencyid, resourceid, version, dim='', representation='', granularity='', fields='', lang='es', as_dataframe=T) {
   path = paste('datasets', agencyid, resourceid, version, sep = "/")
-  url = build_entrypoint_url(STATISTICAL_RESOURCES_API, path, query_list = list(dim=dim, fields=fields, lang=lang))
+  url = build_entrypoint_url(STATISTICAL_RESOURCES_API, path, query_list = list(dim=dim, representation=representation, granularity=granularity, fields=fields, lang=lang))
   api_response = get_content(url)
   if(as_dataframe) {
     return(build_resolved_api_response(api_response))
